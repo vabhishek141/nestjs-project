be38c43aa00ccbb629f44360e40dc3f6249f4b3c
Abhishek Verma <abhishek.verma@talentica.com>
2023-11-06 21:40:03 +0530

    first example

---
+++ Example1/src/main.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, Module } from "@nestjs/common";
+import {NestFactory} from "@nestjs/core"
+
+
+@Controller()
+class AppController {
+
+@Get()
+getRootRoute(){
+return 'hi there!';
+}
+}
+
+@Module({
+    controllers:[AppController]
+})
+class AppModule{}
+
+async function bootstrap(){
+    const app = await NestFactory.create(AppModule);
+
+    await app.listen(3000);
+}
+
+bootstrap();
